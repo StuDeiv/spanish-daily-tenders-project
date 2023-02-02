deff3c720361f0a14e077fb8dab39adbd66978b4
StuDeiv <90849919+StuDeiv@users.noreply.github.com>
2023-02-02 17:11:16 +0100

    add check date function before scraping

--- scraping/utils.js
+++ scraping/utils.js
@@ -1,8 +1,11 @@
 import * as xml2js from 'xml2js'
+import { UPDATED_DATE } from '../db/index.js'
+import { writeDBFile } from '../db/index.js'
+import { logInfo, logSuccess, logError } from './log.js'
 // Disable SSL certificate validation for development purposes
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
-export const SCRAPING_URL = 'https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3.atom'
+const SCRAPING_URL = 'https://contrataciondelsectorpublico.gob.es/sindicacion/sindicacion_643/licitacionesPerfilesContratanteCompleto3.atom'
 
 const parserOptions = {
     attrkey: 'attr',
@@ -15,6 +18,11 @@ const parserOptions = {
     trim: true,
     valueProcessors: [xml2js.processors.parseNumbers, xml2js.processors.parseBooleans],
 }
+
+let res = await fetch(SCRAPING_URL)
+let body = await res.text()
+const parser = new xml2js.Parser(parserOptions)
+let result = await parser.parseStringPromise(body)
 
 const getActualDate = () => {
     const date = new Date()
@@ -29,13 +37,9 @@ const getActualDate = () => {
 }
 
 export async function getDailyTendersAndInfo(){
-    let res = await fetch(SCRAPING_URL)
-    let body = await res.text()
-    const parser = new xml2js.Parser(parserOptions)
 
     let deletedTenders = []
     let updatedDate = '' 
-    let result = await parser.parseStringPromise(body)
     let nextLink = result['link'].filter((link) => link['rel'] === 'next')[0]['href']
 
     //Get tenders of the main page
@@ -54,4 +58,39 @@ export async function getDailyTendersAndInfo(){
         if(!nextLink.includes(getActualDate())) nextLink = null
     }
     return {tenders, deletedTenders, updatedDate}
+}
+
+export function isLinkUpdated(){
+    let updatedDate = result['updated']
+    return updatedDate === UPDATED_DATE
+}
+
+export async function scrapeAndSaveDB(){
+    const start = performance.now()
+
+    try {
+    	logInfo(`Start scraping...`)
+    	const { tenders, deletedTenders, updatedDate } = await getDailyTendersAndInfo()
+    	logSuccess(`Scraped successfully`)
+
+    	logInfo(`Writing [tenders] to database...`)
+    	await writeDBFile('tenders', tenders)
+    	logSuccess(`[tenders] written successfully`)
+
+    	logInfo(`Writing [deleted-tenders] to database...`)
+    	await writeDBFile('deleted-tenders', deletedTenders)
+    	logSuccess(`[deletedTenders] written successfully`)
+
+    	logInfo(`Writing [updated-date] to database...`)
+    	await writeDBFile('updated-date', updatedDate)
+    	logSuccess(`[updated-date] written successfully`)
+
+    } catch (e) {
+    	logError(`Error scraping`)
+    	logError(e)
+    } finally {
+    	const end = performance.now()
+    	const time = (end - start) / 1000
+    	logInfo(`Scraped in ${time} seconds`)
+    }
 }
